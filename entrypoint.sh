461f470e9e98135505f5b043e08ae76a456d2de9
odoedkiy <osowskit@hotmail.com>
2018-10-03 16:25:15 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 # Validate `--app` parameter
-
+if [ -z "$HEROKU_APP"]
+then
+  # ORG not set
+  echo "Environment Variable required: HEROKU_APP"
+  exit 1
+fi
 # Validate process type arguement (web, worker, etc.)
 
-eval heroku container:release --app $HEROKU_APP $*
+heroku container:login 
+heroku container:push --app $HEROKU_APP $HEROKU_PROCESS_NAME
+eval heroku container:release --app $HEROKU_APP $HEROKU_PROCESS_NAME
